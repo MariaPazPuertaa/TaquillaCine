d4dc231e0af4d36f6d885ce11b06d84e4c85525d
María Paz Puerta Acevedo <mariap.puertaa@autonoma.edu.co>
2025-04-03 23:39:23 -0500

    Se crearon las clases PrimerFuncion, FuncionTarde y FuncionNoche

---
+++ src/autonoma/TaquillaCine/models/FuncionNoche.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.TaquillaCine.models;
+
+/**
+ *
+ * @author Maria Paz Puerta Acevedo <mariaáp.puertaa@autonoma.edu.co>
+ * @since 20250331
+ * @version 1.0.0
+ * 
+ */
+public class FuncionNoche {
+    
+}
