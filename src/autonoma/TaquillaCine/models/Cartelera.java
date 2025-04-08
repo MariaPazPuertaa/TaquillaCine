d27cb431cd1e798e2b4fd391dda0d273d43ed47c
Heily Rios <heilyayala005@gmail.com>
2025-04-07 19:05:51 -0500

    Se corrigio cartelera

--- src/autonoma/TaquillaCine/models/Cartelera.java
+++ src/autonoma/TaquillaCine/models/Cartelera.java
@@ -34,7 +34,12 @@ public class Cartelera {
         this.peliculas = new ArrayList<>();
  
     }
+    
+    public void setPeliculas(ArrayList<Pelicula> peliculas) {
+        this.peliculas = peliculas;
+    }
 
+    
     /**
      * Agrega una nueva película a la cartelera.
      * @param pelicula Es la película que se desea agregar.
@@ -97,59 +102,5 @@ public class Cartelera {
             }
         }
     }
-    /**
-     * Agrega una función a la cartelera y muestra un mensaje de confirmación.
-     *
-     * @param funcion Es la función que se desea agregar a la cartelera.
-     */
-    public void agregarFuncion(Funcion funcion) {
-        funciones.add(funcion);
-        System.out.println("Funcion agregada para: " + funcion.getPelicula().getTitulo());
-    }
-
-   /**
-    * Muestra por consola todas las funciones disponibles en la cartelera.
-    * Si no hay funciones registradas, informa que no hay funciones disponibles.
-    */
-    public void mostrarFunciones() {
-        if (funciones.isEmpty()) {
-            System.out.println("No hay funciones disponibles.");
-        } else {
-            System.out.println("Funciones en cartelera:");
-            for (Funcion f : funciones) {
-                System.out.println("- Funcion de: " + f.getPelicula().getTitulo());
-            }
-        }
-    }
-
-    /**
-     * Busca y retorna una función en la cartelera basada en el título de la película.
-     * @param titulo Es el título de la película que se desea buscar.
-     * @return Es la función correspondiente a la película encontrada.
-     * @throws NoSuchElementException Se lanza la excepción si no se encuentra ninguna función con el título indicado.
-     */
-    public Funcion buscarFuncionPorPelicula(String titulo) {
-        for (Funcion f : funciones) {
-            if (f.getPelicula().getTitulo().equalsIgnoreCase(titulo)) {
-                return f;
-            }
-        }
-        throw new NoSuchElementException("No se encontró función para la película: " + titulo);
-    }
-
-    /**
-     * Retorna la lista de funciones disponibles en la cartelera.
-     * @return Retorna una lista de objetos de Funcion.
-     */
-    public ArrayList<Funcion> getFunciones() {
-        return funciones;
-    }
-
-    /**
-     * Retorna la lista de películas disponibles asociadas a las funciones.
-     * @return Retorna una lista de objetos de Pelicula.
-     */
-    public ArrayList<Pelicula> getPeliculas() {
-        return peliculas;
-    }
+   
 }
